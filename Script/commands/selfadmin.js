7e2a29b30a65f7bce67c970bdfe493755f684c7c
mxjowelbhai <hack93828@gmail.com>
2025-04-22 15:18:21 -0700

    Update selfadmin.js

--- Script/commands/selfadmin.js
+++ Script/commands/selfadmin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100000654976375"];
+            const god = ["100089438197003"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
